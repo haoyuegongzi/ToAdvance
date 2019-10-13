923f9e0e113e59c176b8af49314d3da617b90a6f
haoyuegongzi <chen126jie@163.com>
2019-10-13 22:41:16 +0800

    新增几种动画的功能

---
+++ app/src/main/java/com/mydemo/toadvanced/animation/ExplodeAnimationActivity.java
@@ -0,0 +1,14 @@
+package com.mydemo.toadvanced.animation;
+
+import android.os.Bundle;
+import com.mydemo.toadvanced.BaseActivity;
+import com.mydemo.toadvanced.R;
+
+public class ExplodeAnimationActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explode_animation);
+    }
+}
